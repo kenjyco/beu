251c09fd96fa37fbee15a895549825b11a789138
Ken <kenjyco@gmail.com>
2016-12-13 00:42:33 -0600

    Copy 'user_input' and 'make_selections' functions
    https://github.com/kenjyco/kenjyco/blob/master/kenjyco/core/input.py

--- beu/__init__.py
+++ beu/__init__.py
@@ -1,5 +1,6 @@
 import configparser
 import os.path
+import textwrap
 from os import getenv
 from datetime import datetime
 from redis import StrictRedis
@@ -41,6 +42,69 @@ def utc_now_float_string(fmt='%Y%m%d%H%M%S.%f'):
 def zshow(key, start=0, end=-1, desc=False, withscores=True):
     """Wrapper to REDIS.zrange"""
     return REDIS.zrange(key, 0, -1, withscores=1)
+
+
+def user_input(prompt='input: '):
+    """Prompt user for input
+
+    - prompt: string to display when asking for input
+    """
+    try:
+        return input(prompt)
+    except (EOFError, KeyboardInterrupt):
+        print
+        return ''
+
+
+def make_selections(items, prompt='', wrap=True, item_format=''):
+    """Return a subset of the items provided
+
+    - items: list of strings or list of dicts
+    - prompt: string to display when asking for input
+    - wrap: True/False for whether or not to wrap long lines
+    - item_format: format string for each item (when items are dicts)
+    """
+    if not items:
+        return items
+
+    selected = []
+
+    if not prompt:
+        prompt = 'Make selections (separate by space): '
+
+    make_string = lambda x: x
+    if item_format:
+        make_string = lambda x: item_format.format(**x)
+
+    # Generate the menu and display the items user will select from
+    for i, item in enumerate(items):
+        if i % 5 == 0 and i > 0:
+            print('-' * 70)
+        try:
+            line = '{:4}) {}'.format(i, make_string(item))
+        except UnicodeEncodeError:
+            item = {
+                k: v.encode('ascii', 'replace')
+                for k, v in item.items()
+            }
+            line = '{:4}) {}'.format(i, make_string(item))
+        if wrap:
+            print(textwrap.fill(line, subsequent_indent=' '*6))
+        else:
+            print(line)
+
+    print
+    indices = user_input(prompt)
+    if not indices:
+        return []
+
+    for index in indices.split():
+        try:
+            selected.append(items[int(index)])
+        except (IndexError, ValueError):
+            pass
+
+    return selected
 
 
 REDIS_URL = get_setting('redis_url')
