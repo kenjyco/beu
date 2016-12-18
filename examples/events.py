5be7ce54144e57e3eba3e30f3f2f04f7ba01acf9
Ken <kenjyco@gmail.com>
2016-12-18 14:05:17 -0600

    Update 'examples/events.py' to index more fields and generate more data
    Also add 'generate_and_add_events' func

--- examples/events.py
+++ examples/events.py
@@ -1,8 +1,10 @@
 import beu
 import random
+import time
+from pprint import pprint
 
 
-QueryEvent = beu.RedThing('ev', 'query', index_fields=['name'], json_fields=['data'])
+QueryEvent = beu.RedThing('ev', 'query', index_fields=['name', 'type'], json_fields=['data'])
 
 
 fake_dict_keys = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh']
@@ -11,15 +13,23 @@ try:
         text = fp.read()
         words = text.split('\n')
 except FileNotFoundError:
-    words = fake_dict_keys[:]
+    words = fake_dict_keys[:] * 50
 
 
 def generate_event(send=True):
     event_data = {
-        'name': random.choice(['google', 'yahoo', 'askjeeves']),
+        'name': random.choice(['google', 'yahoo', 'askjeeves', 'altavista']),
+        'type': random.choice([
+            'tv shows', 'sports', 'movies', 'howtos', 'tutorials', 'news', 'articles',
+            'gifs', 'vids', 'politics', 'business', 'science', 'health',
+        ]),
         'query': ' '.join(random.sample(words, random.randint(2, 7))),
+        'description': ' '.join(random.sample(words, random.randint(20, 70))),
         'data': {
-            random.choice(fake_dict_keys): random.randint(50, 5000)
+            random.choice(fake_dict_keys): [
+                random.randint(50, 5000)
+                for __ in range(random.randint(3, 6))
+            ]
             for _ in range(random.randint(1, 4))
         },
     }
@@ -29,3 +39,14 @@ def generate_event(send=True):
         event_id = QueryEvent.add(**event_data)
 
     return (event_id, event_data)
+
+
+def generate_and_add_events(n):
+    initial_used_memory = beu.REDIS.info()['used_memory_human']
+    start = time.time()
+    for _ in range(n):
+        generate_event(send=True)
+    end = time.time()
+    final_used_memory = beu.REDIS.info()['used_memory_human']
+    print('Added {} events in {} seconds'.format(n, end - start))
+    print('Memory usage went from {} to {}'.format(initial_used_memory, final_used_memory))
