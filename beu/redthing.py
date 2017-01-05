013486d211b9cb620b17457b5ab3ad2f6c7b2ac3
Ken <kenjyco@gmail.com>
2017-01-05 00:51:51 -0600

    Use assert instead of Exception in RedThing.__init__

--- beu/redthing.py
+++ beu/redthing.py
@@ -33,9 +33,7 @@ class RedThing(beu.RedKeyMaker):
             .union(index_fields.intersection(self._pickle_fields))
             .union(self._json_fields.intersection(self._pickle_fields))
         )
-        if invalid != set():
-            msg = 'The following field(s) used in too many places: {}'
-            raise Exception(msg.format(invalid))
+        assert invalid == set(), 'field(s) used in too many places: {}'.format(invalid)
 
         self._base_key = self._make_key(namespace, name)
         self._index_base_keys = {
