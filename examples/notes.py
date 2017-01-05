c21aea0ef6ab1cd45e2c7bf067587fd5b895b6af
Ken <kenjyco@gmail.com>
2017-01-05 01:21:03 -0600

    Add 'notes.py' and 'prompts.py' to examples dir

---
+++ examples/notes.py
@@ -0,0 +1,17 @@
+import random
+import beu
+from pprint import pprint
+
+notes = beu.RedThing('ns', 'note', index_fields='tag')
+
+
+def input_session(prompt='> '):
+    while True:
+        text = beu.user_input(prompt)
+        if text:
+            notes.add(text=text)
+        else:
+            break
+
+
+input_session()
---
+++ examples/prompts.py
@@ -0,0 +1,23 @@
+import random
+import beu
+from pprint import pprint
+
+prompts = beu.RedThing('ns', 'prompt', index_fields='p,tag')
+prompt_strings = [
+    'basketball player', 'super hero', 'favorite food', 'video game',
+    'movie', 'tv show'
+]
+
+
+def prompt_session(prompt_string=None, ch='> '):
+    prompt_string = prompt_string or random.choice(prompt_strings)
+    while True:
+        text = beu.user_input(prompt_string + ch)
+        if text:
+            prompts.add(text=text, p=prompt_string)
+        else:
+            break
+
+
+print('\nPress <Enter> twice to stop prompting.\n')
+prompt_session()
