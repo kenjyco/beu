94abff135fff63e0550daa442a52ffde0798db9f
Ken <kenjyco@gmail.com>
2017-01-05 00:49:21 -0600

    Fix bug in links.py example

--- examples/links.py
+++ examples/links.py
@@ -1,5 +1,7 @@
 import beu
 
 link_collection = beu.UniqueRedThing('vs', 'link')
-link_collection.add(link='https://blah.net', domain='blah.net')
-link_collection.add(link='https://blah.net?stuff=1&this=that', domain='blah.net')
+
+if link_collection.size == 0:
+    link_collection.add(link='https://blah.net', domain='blah.net')
+    link_collection.add(link='https://blah.net?stuff=1&this=that', domain='blah.net')
