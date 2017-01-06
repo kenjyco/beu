74f073b3a0f3fbdaaa33c3ffa8adfbc5049d4291
Ken <kenjyco@gmail.com>
2017-01-05 23:19:14 -0600

    In examples/__init__.py just import the interesting objects from others

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,9 +1,11 @@
-__all__ = ['events', 'links', 'notes', 'prompts']
-
-import examples.events as events
-import examples.links as links
-import examples.notes as notes
-import examples.prompts as prompts
+from examples.events import (
+    QueryEvent, generate_event, generate_and_add_events, slow_trickle_events, now
+)
+from examples.notes import notes, input_session, hand_pick_from
+from examples.links import link_collection
+from examples.prompts import prompts, prompt_session
 
 if __name__ == '__main__':
-    print('\nLoaded the following examples: {}'.format(', '.join(__all__)))
+    import beu
+    from pprint import pprint
+    print('\nUse dir() to see what is available')
