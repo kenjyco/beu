395604d81718108ff68c02c441e38477cd4b8330
Ken <kenjyco@gmail.com>
2017-01-10 09:43:14 -0600

    Use 'links' instead of 'link_collection'

--- examples/__init__.py
+++ examples/__init__.py
@@ -2,8 +2,8 @@ from examples.events import (
     events, generate_event, generate_and_add_events, slow_trickle_events, now
 )
 from examples.notes import notes, input_session, hand_pick_from
-from examples.links import link_collection
 from examples.prompts import prompts, prompt_session
+from examples.links import links
 
 if __name__ == '__main__':
     import beu
--- examples/links.py
+++ examples/links.py
@@ -1,9 +1,9 @@
 import beu
 
-link_collection = beu.UniqueRedThing('vs', 'link')
+links = beu.UniqueRedThing('vs', 'link')
 
 
 if __name__ == '__main__':
-    if link_collection.size == 0:
-        link_collection.add(link='https://blah.net', domain='blah.net')
-        link_collection.add(link='https://blah.net?stuff=1&this=that', domain='blah.net')
+    if links.size == 0:
+        links.add(link='https://blah.net', domain='blah.net')
+        links.add(link='https://blah.net?stuff=1&this=that', domain='blah.net')
