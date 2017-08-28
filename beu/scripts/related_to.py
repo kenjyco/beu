b81d73a31b5330cbe41b83ca5c084629a457c5a2
Ken <kenjyco@gmail.com>
2017-08-27 19:39:50 -0500

    Add '--mp3' option to 'related_to' for audio only downloads

--- beu/scripts/related_to.py
+++ beu/scripts/related_to.py
@@ -3,8 +3,12 @@ import beu
 
 
 @click.command()
+@click.option(
+    '--mp3', '-m', 'mp3', is_flag=True, default=False,
+    help='Convert downloaded to MP3 file'
+)
 @click.argument('url', nargs=1, default='')
-def main(url):
+def main(url, **kwargs):
     """Find and download vids related to vid url"""
     url = url or beu.ih.user_input('vid url')
     if not url:
@@ -16,5 +20,16 @@ def main(url):
         item_format='{duration} .::. {title} .::. {user}',
     )
     if selected:
-        results = [beu.yh.av_from_url(x['link'], playlist=True) for x in selected]
+        av_from_url_kwargs = {'playlist': True}
+        if kwargs and kwargs.get('mp3') is True:
+            av_from_url_kwargs.update({
+                'audio_only': True,
+                'mp3': True,
+            })
+        results = [
+            beu.yh.av_from_url(
+                x['link'],
+                **av_from_url_kwargs
+            ) for x in selected
+        ]
         return results
