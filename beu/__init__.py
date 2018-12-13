cab6de6c98f5d12af1771b13c71b40df2971c248
Ken <kenjyco@gmail.com>
2018-12-13 14:36:29 -0600

    Use fs_helper.get_logger func

--- beu/__init__.py
+++ beu/__init__.py
@@ -15,20 +15,6 @@ except ImportError:
 import moc
 import mocp_cli
 import chloop
-import logging
-import os.path
 
 
-LOGFILE = os.path.abspath(os.path.expanduser('~/logs/beu.log'))
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler(LOGFILE, mode='a')
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(logging.Formatter(
-    '%(asctime)s - %(levelname)s - %(funcName)s: %(message)s'
-))
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-console_handler.setFormatter(logging.Formatter('%(asctime)s: %(message)s'))
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
+logger = fh.get_logger(__name__)
