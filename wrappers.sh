0d82992cdf89901f07dc71557b79448499ada38e
Ken <kenjyco@gmail.com>
2018-12-27 13:32:34 -0600

    Update beu-update and beu-repos-update to use array for pip_args (zsh)

--- wrappers.sh
+++ wrappers.sh
@@ -23,17 +23,17 @@ beu-update() {
     [[ ! -d "$HOME/.beu/venv" ]] && echo "$HOME/.beu/venv does not exist" && return 1
     oldpwd=$(pwd)
     cd $HOME/.beu
-    pip_args="--upgrade"
+    pip_args=(--upgrade)
     pip_version=$(venv/bin/pip3 --version | egrep -o 'pip (\d+)' | cut -c 5-)
     [[ -z "$pip_version" ]] && pip_version=$(venv/bin/pip3 --version | perl -pe 's/^pip\s+(\d+).*/$1/')
     [[ -z "$pip_version" ]] && pip_version=0
     if [[ $pip_version -gt 9 ]]; then
-        pip_args=" --upgrade --upgrade-strategy=eager"
+        pip_args=(--upgrade --upgrade-strategy eager)
     fi
     if [[ $(uname) == 'Darwin' ]]; then
-        venv/bin/pip3 install $pip_args beu
+        venv/bin/pip3 install ${pip_args[@]} beu
     else
-        venv/bin/pip3 install $pip_args beu vlc-helper
+        venv/bin/pip3 install ${pip_args[@]} beu vlc-helper
     fi
     echo -e "\nSaving latest wrappers.sh"
     curl https://raw.githubusercontent.com/kenjyco/beu/master/wrappers.sh > wrappers.sh
@@ -170,17 +170,17 @@ beu-repos-setup() {
     for repo in $(beu-repos-list | tr '\n' '\0' | xargs -0); do
 		cd "$repo"
         echo -e "\n--------------------------------------------------\n$repo"
-		pip_args="--upgrade"
+		pip_args=(--upgrade)
         if [[ ! -d "venv" ]]; then
 			echo "Creating venv at $(pwd)/venv..."
-			python3 -m venv venv && venv/bin/pip3 install pip wheel $pip_args
+			python3 -m venv venv && venv/bin/pip3 install pip wheel ${pip_args[@]}
 		fi
 		pip_version=$(venv/bin/pip3 --version | egrep -o 'pip (\d+)' | cut -c 5-)
 		[[ -z "$pip_version" ]] && pip_version=$(venv/bin/pip3 --version | perl -pe 's/^pip\s+(\d+).*/$1/')
 		[[ -z "$pip_version" ]] && pip_version=0;
-		[[ $pip_version -gt 9 ]] && pip_args=" --upgrade --upgrade-strategy=eager"
-		[[ -f requirements.txt ]] && venv/bin/pip3 install -r requirements.txt $pip_args
-		venv/bin/pip3 install ipython pdbpp $pip_args
+        [[ $pip_version -gt 9 ]] && pip_args=(--upgrade --upgrade-strategy eager)
+		[[ -f requirements.txt ]] && venv/bin/pip3 install -r requirements.txt ${pip_args[@]}
+		venv/bin/pip3 install ipython pdbpp ${pip_args[@]}
     done
     cd "$oldpwd"
 }
