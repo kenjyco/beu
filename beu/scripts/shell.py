30e6d8cf910a0379885b2e465c41ff580e1ca02b
Ken <kenjyco@gmail.com>
2019-03-19 12:52:51 -0500

    Update beu-ipython command to use shell colors

--- beu/scripts/shell.py
+++ beu/scripts/shell.py
@@ -5,9 +5,12 @@ import click
 def main():
     """Start ipython with `beu` imported"""
     from IPython import embed
+    from traitlets.config import get_config
     import beu
     from pprint import pprint
-    embed()
+    c = get_config()
+    c.InteractiveShellEmbed.colors = "Linux"
+    embed(config=c)
 
 
 if __name__ == '__main__':
