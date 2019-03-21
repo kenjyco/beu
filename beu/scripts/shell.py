9daf6a09e95cb740a2d56acdd3f2ade8f81b1ac8
Ken <kenjyco@gmail.com>
2019-03-21 05:16:06 -0500

    Update beu-ipython command to use vi editing mode

--- beu/scripts/shell.py
+++ beu/scripts/shell.py
@@ -5,11 +5,12 @@ import click
 def main():
     """Start ipython with `beu` imported"""
     from IPython import embed
-    from traitlets.config import get_config
+    from traitlets.config import Config
     import beu
     from pprint import pprint
-    c = get_config()
+    c = Config()
     c.InteractiveShellEmbed.colors = "Linux"
+    c.InteractiveShellEmbed.editing_mode = "vi"
     embed(config=c)
 
 
