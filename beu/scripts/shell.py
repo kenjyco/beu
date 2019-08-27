750f22b52fd471f97f5817aecf0bb909af4c4210
Ken <kenjyco@gmail.com>
2019-08-27 04:29:42 -0500

    Use beu.ih.start_ipython func in beu-ipython and allow disabling vi/colors

--- beu/scripts/shell.py
+++ beu/scripts/shell.py
@@ -2,16 +2,24 @@ import click
 
 
 @click.command()
-def main():
-    """Start ipython with `beu` imported"""
-    from IPython import embed
-    from traitlets.config import Config
+@click.option(
+    '--no-vi', 'no_vi', is_flag=True, default=False,
+    help='Do not use vi editing mode'
+)
+@click.option(
+    '--no-colors', 'no_colors', is_flag=True, default=False,
+    help='Do not use colors / syntax highlighting'
+)
+def main(**kwargs):
+    """Start ipython with `beu` and `pprint` imported"""
     import beu
     from pprint import pprint
-    c = Config()
-    c.InteractiveShellEmbed.colors = "Linux"
-    c.InteractiveShellEmbed.editing_mode = "vi"
-    embed(config=c)
+    beu.ih.start_ipython(
+        colors=not kwargs['no_colors'],
+        vi=not kwargs['no_vi'],
+        beu=beu,
+        pprint=pprint
+    )
 
 
 if __name__ == '__main__':
