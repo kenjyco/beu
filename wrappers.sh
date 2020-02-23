6831363a688a5620e243effd7fb0b97e44559200
Ken <kenjyco@gmail.com>
2020-02-23 13:52:31 -0600

    Alphabetize repos/packages where they appear in project

--- wrappers.sh
+++ wrappers.sh
@@ -51,28 +51,28 @@ beu-repos-list() {
     find ~ -maxdepth $level -path ~/Library -prune -o -type d -name ".git" -print0 |
     xargs -0 -I {} dirname {} 2>/dev/null |
     sort |
-    egrep '(beu|bg-helper|chloop|input-helper|mocp|parse-helper|redis-helper|vlc-helper|yt-helper|dt-helper|fs-helper|settings-helper|jira-helper|aws-info-helper|easy-workflow-manager|mongo-helper|sql-helper)'
+    egrep '(aws-info-helper|beu|bg-helper|chloop|dt-helper|easy-workflow-manager|fs-helper|input-helper|jira-helper|mocp|mongo-helper|parse-helper|redis-helper|settings-helper|sql-helper|vlc-helper|yt-helper)'
 }
 
 BEU_REPOS_LIST=$(beu-repos-list)
+_REPO_AWS_INFO_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep aws-info-helper$)
 _REPO_BEU=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep beu$)
 _REPO_BG_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep bg-helper$)
 _REPO_CHLOOP=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep chloop$)
+_REPO_DT_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep dt-helper$)
+_REPO_EASY_WORKFLOW_MANAGER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep easy-workflow-manager$)
+_REPO_FS_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep fs-helper$)
 _REPO_INPUT_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep input-helper$)
+_REPO_JIRA_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep jira-helper$)
 _REPO_MOCP=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep mocp$)
 _REPO_MOCP_CLI=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep mocp-cli$)
+_REPO_MONGO_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep mongo-helper$)
 _REPO_PARSE_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep parse-helper$)
 _REPO_REDIS_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep redis-helper$)
+_REPO_SETTINGS_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep settings-helper$)
+_REPO_SQL_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep sql-helper$)
 _REPO_VLC_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep vlc-helper$)
 _REPO_YT_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep yt-helper$)
-_REPO_DT_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep dt-helper$)
-_REPO_FS_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep fs-helper$)
-_REPO_SETTINGS_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep settings-helper$)
-_REPO_JIRA_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep jira-helper$)
-_REPO_AWS_INFO_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep aws-info-helper$)
-_REPO_EASY_WORKFLOW_MANAGER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep easy-workflow-manager$)
-_REPO_MONGO_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep mongo-helper$)
-_REPO_SQL_HELPER=$(echo $BEU_REPOS_LIST | tr ' ' '\n' | grep sql-helper$)
 
 _get_beu_repos_base_dir() {
     for repo in $(beu-repos-list | tr '\n' '\0' | xargs -0); do
@@ -91,24 +91,24 @@ which colordiff &>/dev/null
 
 _beu-repos-diff-with-site-packages() {
     [[ -z "$BEU_REPOS_LIST" ]] && return
+    [[ -d "$_REPO_AWS_INFO_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/aws_info_helper" "$_REPO_AWS_INFO_HELPER/aws_info_helper"
     [[ -d "$_REPO_BEU" ]] && diff -r "$BEU_SITE_PACKAGES/beu" "$_REPO_BEU/beu"
     [[ -d "$_REPO_BG_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/bg_helper" "$_REPO_BG_HELPER/bg_helper"
     [[ -d "$_REPO_CHLOOP" ]] && diff -r "$BEU_SITE_PACKAGES/chloop" "$_REPO_CHLOOP/chloop"
+    [[ -d "$_REPO_DT_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/dt_helper" "$_REPO_DT_HELPER/dt_helper"
+    [[ -d "$_REPO_EASY_WORKFLOW_MANAGER" ]] && diff -r "$BEU_SITE_PACKAGES/easy_workflow_manager" "$_REPO_EASY_WORKFLOW_MANAGER/easy_workflow_manager"
+    [[ -d "$_REPO_FS_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/fs_helper" "$_REPO_FS_HELPER/fs_helper"
     [[ -d "$_REPO_INPUT_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/input_helper" "$_REPO_INPUT_HELPER/input_helper"
+    [[ -d "$_REPO_JIRA_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/jira_helper" "$_REPO_JIRA_HELPER/jira_helper"
     [[ -d "$_REPO_MOCP" ]] && diff -r "$BEU_SITE_PACKAGES/moc" "$_REPO_MOCP/moc"
     [[ -d "$_REPO_MOCP_CLI" ]] && diff -r "$BEU_SITE_PACKAGES/mocp_cli" "$_REPO_MOCP_CLI/mocp_cli"
+    [[ -d "$_REPO_MONGO_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/mongo_helper" "$_REPO_MONGO_HELPER/mongo_helper"
     [[ -d "$_REPO_PARSE_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/parse_helper" "$_REPO_PARSE_HELPER/parse_helper"
     [[ -d "$_REPO_REDIS_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/redis_helper" "$_REPO_REDIS_HELPER/redis_helper"
+    [[ -d "$_REPO_SETTINGS_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/settings_helper" "$_REPO_SETTINGS_HELPER/settings_helper"
+    [[ -d "$_REPO_SQL_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/sql_helper" "$_REPO_SQL_HELPER/sql_helper"
     [[ -d "$_REPO_VLC_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/vlc_helper" "$_REPO_VLC_HELPER/vlc_helper"
     [[ -d "$_REPO_YT_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/yt_helper" "$_REPO_YT_HELPER/yt_helper"
-    [[ -d "$_REPO_DT_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/dt_helper" "$_REPO_DT_HELPER/dt_helper"
-    [[ -d "$_REPO_FS_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/fs_helper" "$_REPO_FS_HELPER/fs_helper"
-    [[ -d "$_REPO_SETTINGS_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/settings_helper" "$_REPO_SETTINGS_HELPER/settings_helper"
-    [[ -d "$_REPO_JIRA_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/jira_helper" "$_REPO_JIRA_HELPER/jira_helper"
-    [[ -d "$_REPO_AWS_INFO_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/aws_info_helper" "$_REPO_AWS_INFO_HELPER/aws_info_helper"
-    [[ -d "$_REPO_EASY_WORKFLOW_MANAGER" ]] && diff -r "$BEU_SITE_PACKAGES/easy_workflow_manager" "$_REPO_EASY_WORKFLOW_MANAGER/easy_workflow_manager"
-    [[ -d "$_REPO_MONGO_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/mongo_helper" "$_REPO_MONGO_HELPER/mongo_helper"
-    [[ -d "$_REPO_SQL_HELPER" ]] && diff -r "$BEU_SITE_PACKAGES/sql_helper" "$_REPO_SQL_HELPER/sql_helper"
     if [[ -s "$_REPO_BEU/wrappers.sh" ]]; then
         _output=$(diff "$HOME/.beu/wrappers.sh" "$_REPO_BEU/wrappers.sh")
         [[ -n "$_output" ]] && echo diff "$HOME/.beu/wrappers.sh" "$_REPO_BEU/wrappers.sh" && echo "$_output"
