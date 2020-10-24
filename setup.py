8b73764ac6358160dbd97281373eddd8fbcd192d
Ken <kenjyco@gmail.com>
2020-10-24 15:02:58 -0500

    Bump to v0.1.33

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.rst', 'r') as fp:
 
 setup(
     name='beu',
-    version='0.1.32',
+    version='0.1.33',
     description='Beginner Express .:. Back End .:. Big Example .:. Brainstorm Effectively',
     long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/beu',
-    download_url='https://github.com/kenjyco/beu/tarball/v0.1.32',
+    download_url='https://github.com/kenjyco/beu/tarball/v0.1.33',
     packages=find_packages(),
     install_requires=[
         'aws-info-helper',
