484da9833fa12a446a20ddf635ecd90d78d8e043
Ken <kenjyco@gmail.com>
2021-11-16 15:13:28 -0600

    Update wrappers.sh to get BEU_SITE_PACKAGES with v3.9

--- wrappers.sh
+++ wrappers.sh
@@ -6,6 +6,8 @@ elif [[ -d "$HOME/.beu/venv/lib/python3.7/site-packages" ]]; then
     BEU_SITE_PACKAGES="$HOME/.beu/venv/lib/python3.7/site-packages"
 elif [[ -d "$HOME/.beu/venv/lib/python3.8/site-packages" ]]; then
     BEU_SITE_PACKAGES="$HOME/.beu/venv/lib/python3.8/site-packages"
+elif [[ -d "$HOME/.beu/venv/lib/python3.9/site-packages" ]]; then
+    BEU_SITE_PACKAGES="$HOME/.beu/venv/lib/python3.9/site-packages"
 elif [[ -d "$HOME/.beu/venv/Lib/site-packages" ]]; then
     BEU_SITE_PACKAGES="$HOME/.beu/venv/Lib/site-packages"
 fi
