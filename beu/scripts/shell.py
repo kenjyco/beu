7f8c5edb2bf7cb6fc29f9a66bb22f862440a907b
Ken <kenjyco@gmail.com>
2023-06-22 00:23:11 -0500

    Update help text and add --confirm-exit option to beu-ipython script

--- beu/scripts/shell.py
+++ beu/scripts/shell.py
@@ -4,11 +4,15 @@ import click
 @click.command()
 @click.option(
     '--no-vi', 'no_vi', is_flag=True, default=False,
-    help='Do not use vi editing mode'
+    help='Do not use vi editing mode in ipython'
 )
 @click.option(
     '--no-colors', 'no_colors', is_flag=True, default=False,
-    help='Do not use colors / syntax highlighting'
+    help='Do not use colors / syntax highlighting in ipython'
+)
+@click.option(
+    '--confirm-exit', 'confirm_exit', is_flag=True, default=False,
+    help='Prompt "Do you really want to exit ([y]/n)?" when exiting ipython'
 )
 def main(**kwargs):
     """Start ipython with `beu` and `pprint` imported"""
@@ -17,6 +21,7 @@ def main(**kwargs):
     beu.ih.start_ipython(
         colors=not kwargs['no_colors'],
         vi=not kwargs['no_vi'],
+        confirm_exit=kwargs['confirm_exit'],
         beu=beu,
         pprint=pprint
     )
