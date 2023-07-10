8e03a44d1b98ddd31642ec9e817ea9bcc7919650
Ken <kenjyco@gmail.com>
2023-07-09 23:58:36 -0500

    Bump to v0.1.36

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ with open('requirements.txt', 'r') as fp:
 
 setup(
     name='beu',
-    version='0.1.35',
+    version='0.1.36',
     description='Beginner Express .:. Back End .:. Big Example .:. Brainstorm Effectively',
     long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/beu',
-    download_url='https://github.com/kenjyco/beu/tarball/v0.1.35',
+    download_url='https://github.com/kenjyco/beu/tarball/v0.1.36',
     packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
